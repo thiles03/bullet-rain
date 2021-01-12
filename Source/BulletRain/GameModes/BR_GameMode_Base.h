2121171888ef3db10f347039bd8ff96c2b8e9f91
thiles03 <thiles03@gmail.com>
2021-01-12 19:27:36 +0000

    Player Character

---
+++ Source/BulletRain/GameModes/BR_GameMode_Base.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BR_GameMode_Base.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BULLETRAIN_API ABR_GameMode_Base : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
