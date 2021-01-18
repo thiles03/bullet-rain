cb1730fc1d028f5cda8b197b70aa3b59fcec57b9
thiles03 <thiles03@gmail.com>
2021-01-18 21:45:32 +0000

    Refactor stats component

--- Source/BulletRain/Components/BR_CharacterStats.cpp
+++ Source/BulletRain/Components/BR_CharacterStats.cpp
@@ -1,17 +1,17 @@
-#include "BR_CharacterStats.h"
+#include "BR_CharacterStats_Base.h"
 #include "BulletRain/Controllers/BR_PlayerController.h"
 #include "GameFramework/PlayerController.h"
 #include "Math/UnrealMathUtility.h"
 #include "TimerManager.h"
 
 //Constructor
-UBR_CharacterStats::UBR_CharacterStats()
+UBR_CharacterStats_Base::UBR_CharacterStats_Base()
 {
 	PrimaryComponentTick.bCanEverTick = true;
 }
 
 // Called when the game starts
-void UBR_CharacterStats::BeginPlay()
+void UBR_CharacterStats_Base::BeginPlay()
 {
 	Super::BeginPlay();
 	PlayerController = Cast<ABR_PlayerController>(GetWorld()->GetFirstPlayerController());
@@ -20,7 +20,7 @@ void UBR_CharacterStats::BeginPlay()
 }
 
 // Called every frame
-void UBR_CharacterStats::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+void UBR_CharacterStats_Base::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
@@ -32,39 +32,39 @@ void UBR_CharacterStats::TickComponent(float DeltaTime, ELevelTick TickType, FAc
 }
 
 //SETTERS
-void UBR_CharacterStats::SetMaxArmour(float ArmourValue)
+void UBR_CharacterStats_Base::SetMaxArmour(float ArmourValue)
 {
 	MaxArmour = ArmourValue;
 }
 
-void UBR_CharacterStats::UpdateCurrentArmour(float ArmourValue)
+void UBR_CharacterStats_Base::UpdateCurrentArmour(float ArmourValue)
 {
 	CurrentArmour = FMath::Clamp(CurrentArmour + ArmourValue, 0.f, MaxArmour);
 }
 
-void UBR_CharacterStats::SetMaxHealth(float HealthValue)
+void UBR_CharacterStats_Base::SetMaxHealth(float HealthValue)
 {
 	MaxHealth = HealthValue;
 }
 
-void UBR_CharacterStats::UpdateCurrentHealth(float HealthValue)
+void UBR_CharacterStats_Base::UpdateCurrentHealth(float HealthValue)
 {
 	CurrentHealth = FMath::Clamp(CurrentHealth + HealthValue, 0.f, MaxHealth);
 }
 
 //GETTERS
-float UBR_CharacterStats::GetArmourPercent() const
+float UBR_CharacterStats_Base::GetArmourPercent() const
 {
 	return CurrentArmour / MaxArmour;
 }
 
-float UBR_CharacterStats::GetHealthPercent() const
+float UBR_CharacterStats_Base::GetHealthPercent() const
 {
 	return CurrentHealth / MaxHealth;
 }
 
 // Take blockable damage to armour and health
-void UBR_CharacterStats::TakeDamage(float Damage)
+void UBR_CharacterStats_Base::TakeDamage(float Damage)
 {
 	if (Damage <= 0) {return;}
 	float RemainingDamage = CurrentArmour - Damage;
@@ -73,23 +73,23 @@ void UBR_CharacterStats::TakeDamage(float Damage)
 	{
 		UpdateCurrentHealth(RemainingDamage);
 		CanRegen = false;
-		GetOwner()->GetWorldTimerManager().SetTimer(RegenResetTimer, this, &UBR_CharacterStats::EnableRegen, RegenDelay, false);
+		GetOwner()->GetWorldTimerManager().SetTimer(RegenResetTimer, this, &UBR_CharacterStats_Base::EnableRegen, RegenDelay, false);
 		PlayerController->CreateDamageVignette(); //TODO - Only if player character
 	}
 }
 
 // Take blockable damage and unblockable damage
-void UBR_CharacterStats::TakeUnblockableDamage(float Damage, float UnblockableDamage)
+void UBR_CharacterStats_Base::TakeUnblockableDamage(float Damage, float UnblockableDamage)
 {
 	TakeDamage(Damage);
 	CurrentHealth = (FMath::Clamp((CurrentHealth - UnblockableDamage), 0.0f, MaxHealth));
 	CanRegen = false;
-	GetOwner()->GetWorldTimerManager().SetTimer(RegenResetTimer, this, &UBR_CharacterStats::EnableRegen, RegenDelay, false);
+	GetOwner()->GetWorldTimerManager().SetTimer(RegenResetTimer, this, &UBR_CharacterStats_Base::EnableRegen, RegenDelay, false);
 	PlayerController->CreateDamageVignette(); //TODO - Only if player character
 }
 
 // Enable health regeneration
-void UBR_CharacterStats::EnableRegen() 
+void UBR_CharacterStats_Base::EnableRegen() 
 {
 	CanRegen = true;
 }
--- Source/BulletRain/Components/BR_CharacterStats_Base.h
+++ Source/BulletRain/Components/BR_CharacterStats_Base.h
@@ -2,18 +2,18 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
-#include "BR_CharacterStats.generated.h"
+#include "BR_CharacterStats_Base.generated.h"
 
 class ABR_PlayerController;
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class BULLETRAIN_API UBR_CharacterStats : public UActorComponent
+class BULLETRAIN_API UBR_CharacterStats_Base : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
 	// Sets default values for this component's properties
-	UBR_CharacterStats();
+	UBR_CharacterStats_Base();
 
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
